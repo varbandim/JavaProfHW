4ab11446238702ad3fe84354888e4e22074ba775
varbandim <varbandim@yandex.ru>
2017-08-23 00:00:41 +0300

    2

---
+++ src/Lesson3/readFile.java
@@ -0,0 +1,11 @@
+package Lesson3;
+
+public class readFile {
+
+    private static final String passFile = "D:\\!YandexDisk\\geekbrains.ru\\Java. Prof\\HomeWorks\\src\\Lesson3\\10Mb.txt";
+
+    public static void main(String[] args) {
+
+    }
+
+}
