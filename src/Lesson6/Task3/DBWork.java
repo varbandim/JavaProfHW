a6089e13762236030b8c2ad10e375bde1a54df9f
Дмитрий <varbandim@yandex.ru>
2017-09-07 10:51:55 +0300

    Урок 6. Работа над ошибками. 3 задача

--- src/Lesson6/Task3/DBWork.java
+++ src/Lesson6/Task3/DBWork.java
@@ -1,33 +1,61 @@
 package Lesson6.Task3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.*;
 
 public class DBWork {
+    private static Connection connection;
+    private static Statement stmt;
+    private static Savepoint point;
+    private static ResultSet result;
+    //чтение
+    private static final String readSql = "SELECT * FROM Students";
+    //обновление
+    private static final String updateSql = "UPDATE INTO Students SET mark = 15 WHERE name = 'Mark'";
+    //добавление
+    private static final String addSql = "INSERT INTO Students (name, mark) VALUES ('vasya', 18)";
 
-    static Connection connection;
-    static Statement stmt;
-    private String path = "jdbc:sqlite:src/Lesson6/MainDB.db";
 
-    public void connect(String path) {
+    //проинициализировать базу и создать точку возврата
+    @BeforeClass
+    public static void setConnection(){
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection(path);
+            connection = DriverManager.getConnection("jdbc:sqlite:src/Lesson6/Task3/mainDB.db");
             stmt = connection.createStatement();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            point = connection.setSavepoint("Before");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void disconnect() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    @Test
+    public void testBase() throws SQLException {
+        //корректно добавляются
+        result = stmt.executeQuery(addSql);
+        if (!result.next()) {
+            Assert.fail("Данные не добавляются");
         }
+
+        // обновляются
+        result = stmt.executeQuery(updateSql);
+        if (!result.next()) {
+            Assert.fail("Данные не обновляются");
+        }
+
+        //читаются записи
+        result = stmt.executeQuery(readSql);
+        if (!result.next()) {
+            Assert.fail("Данные не читаются");
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.rollback(point);
+        connection.close();
     }
 }
--- src/Lesson6/Task3/TestDB.java
+++ src/Lesson6/Task3/TestDB.java
@@ -1,11 +1,11 @@
 package Lesson6.Task3;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /*
 Написать тесты для
@@ -14,5 +14,51 @@ import java.sql.SQLException;
 тестов эти записи не должны быть удалены/изменены/добавлены.
  */
 public class TestDB {
-    
+   private static Connection connection;
+   private static Statement stmt;
+   private static Savepoint point;
+
+   @BeforeClass
+    public static void initDB(){
+       try{
+           connection = DriverManager.getConnection("jdbc:sqlite:MainDB.db");
+           stmt = connection.createStatement();
+           point = connection.setSavepoint("A");
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+   }
+
+   @Test
+    public void test1() throws SQLException {
+        ResultSet resultSet = stmt.executeQuery("SELECT (*) AS CNT FROM Students");
+        if(!resultSet.next()) Assert.fail("Пустая база данных");
+
+        int startCnt = resultSet.getInt("CNT");
+
+        String sql = "INSERT INTO Students (suname, mark) VALUES ('Hercule', 5)";
+        stmt.executeUpdate(sql);
+       resultSet = stmt.executeQuery("SELECT (*) AS CNT FROM Students");
+       if(!resultSet.next()) {
+           if (startCnt + 1 != resultSet.getInt("CNT")) {
+               Assert.fail("Ошибка добавления данных");
+           }
+       }
+
+       int cntUdated = stmt.executeUpdate("UPDATE Students GET mark = 12 WHERE suranme = 'Hercule'");
+       if (cntUdated == 0) {
+           Assert.fail("Не найдены обновляемые данные");
+       }
+
+       resultSet = stmt.executeQuery("SELECT mark FROM Students WHERE surname = 'Hercule'");
+       resultSet.next();
+       if (resultSet.getInt("mark") != 12) {
+           Assert.fail("Ошибка получения баллов студента");
+       }
+
+       int cntDeleted = stmt.executeUpdate("DELETE FROM Students WHERE surname = 'Hercule'");
+       if (cntDeleted == 0) {
+           Assert.fail("Ошибка удаления данных");
+       }
+   }
 }
